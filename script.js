a5caacdca63800b7f7ef2c5c0749e5bda6021706
arthur-clerc <arthur.clerc@laplateforme.io>
2024-04-22 16:23:49 +0200

    maj script.js

--- script.js
+++ script.js
@@ -1,49 +1,31 @@
 const apiKey = '8c4b867188ee47a1d4e40854b27391ec';
 const apiMovie = 'https://api.themoviedb.org/3/discover/movie?api_key=' + apiKey;
 const apiTv = 'https://api.themoviedb.org/3/discover/tv?api_key=' + apiKey;
+const apiGuestSession = 'https://api.themoviedb.org/3/authentication/guest_session/new?api_key=' + apiKey;
+const apiDeleteSession = 'https://api.themoviedb.org/3/authentication/session?api_key=' + apiKey;
 
-const fetchApiMovie = fetch(apiMovie)
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-})
-.catch(error => {
-    console.error('Une erreur s\'est produite', error);
-});
+async function fetchApi(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Une erreur s\'est produite:', error);
+    }
+}
 
-const fetchApiTv = fetch(apiTv)
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-})
-.catch(error => {
-    console.error('Une erreur s\'est produite', error);
-});
+async function fetchAllApis() {
+    await fetchApi(apiTv);
+    await fetchApi(apiMovie);
+    await fetchApi(apiGuestSession);
+    await fetchApi(apiDeleteSession);
+}
 
-const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const createAccount = 'https://api.themoviedb.org/3/authentication/session/new?api_key=' + apiKey;
-    const isFormValid = () =>
-      [
-        "firstname",
-        "lastname",
-        "pseudo",
-        "birth_date",
-        "email",
-        "password",
-      ].every((input) => data.get(input));
+fetchAllApis();
 
-    const response = await fetch(createAccount, {
-      method: "POST",
-      body: data,
-    });
 
-    const responseData = await response.json();
 
-    if (!response.ok) {
-        console.error(`Erreur`);
-      } else {
-        console.error(responseData);
-      }
-  };
+
+
+
+
