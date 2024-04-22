e93370b62206a925e4035bf7e89768bc3b79439d
arthur-clerc <arthur.clerc@laplateforme.io>
2024-04-22 13:33:00 +0200

    create fetch api movie and tv

---
+++ script.js
@@ -0,0 +1,22 @@
+const apiKey = '8c4b867188ee47a1d4e40854b27391ec';
+const apiMovie = 'https://api.themoviedb.org/3/discover/movie?api_key=' + apiKey;
+const apiTv = 'https://api.themoviedb.org/3/discover/tv?api_key=' + apiKey;
+
+
+const fetchApiMovie = fetch(apiMovie)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+})
+.catch(error => {
+    console.error('Une erreur s\'est produit', error);
+});
+
+const fetchApiTv = fetch(apiTv)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+})
+.catch(error => {
+    console.error('Une erreur s\'est produit', error);
+});
