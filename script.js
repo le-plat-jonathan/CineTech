638c335d5b6ee73bea24e7b7413348c72a70b5a5
jonathan-le-plat <jonathan.le-plat@laplateforme.io>
2024-04-23 09:16:15 +0200

    merging jonathan on main

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const apiKey = '8c4b867188ee47a1d4e40854b27391ec';
 const apiMovie = 'https://api.themoviedb.org/3/discover/movie?api_key=' + apiKey;
 const apiTv = 'https://api.themoviedb.org/3/discover/tv?api_key=' + apiKey;
+const apiGuestSession = 'https://api.themoviedb.org/3/authentication/guest_session/new?api_key=' + apiKey;
+const apiDeleteSession = 'https://api.themoviedb.org/3/authentication/session?api_key=' + apiKey;
 const token = localStorage.getItem('token');
 
 const fetchApiMovie = async () => {
@@ -30,7 +32,7 @@ const guest = document.getElementById('guest');
 
 guest.addEventListener('click', async function() {
   try {
-    const response = await fetch('https://api.themoviedb.org/3/authentication/guest_session/new?api_key=' + apiKey);
+    const response = await fetch(apiGuestSession);
     const data = await response.json();
     localStorage.setItem('token', data.guest_session_id);
     console.log(data);
@@ -38,27 +40,8 @@ guest.addEventListener('click', async function() {
     console.error('Une erreur s\'est produite', error);
 }
 });
-const apiGuestSession = 'https://api.themoviedb.org/3/authentication/guest_session/new?api_key=' + apiKey;
-const apiDeleteSession = 'https://api.themoviedb.org/3/authentication/session?api_key=' + apiKey;
 
-async function fetchApi(url) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error('Une erreur s\'est produite:', error);
-    }
-}
 
-async function fetchAllApis() {
-    await fetchApi(apiTv);
-    await fetchApi(apiMovie);
-    await fetchApi(apiGuestSession);
-    await fetchApi(apiDeleteSession);
-}
-
-fetchAllApis();
 
 
 
